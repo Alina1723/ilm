b061d24ccb0c2bff1bdd808126e2693b4e7e8e9b
Alina1723 <alina.vishi@gmail.com>
2025-10-23 20:50:44 +0100

    added JS
    didn't complete the function yet

---
+++ src/functions.js
@@ -0,0 +1,8 @@
+function changeCityName(event) {
+    event.preventDefault();
+    let cityInput = document.querySelector("#city-input");
+
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit" changeCityName);
