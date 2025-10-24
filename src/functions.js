3a753bb73dbc514b942c068b19b724f8ee42205d
Alina1723 <alina.vishi@gmail.com>
2025-10-24 20:17:00 +0100

    updated js
    the h1 to hate the value from search input

--- src/functions.js
+++ src/functions.js
@@ -1,8 +1,9 @@
 function changeCityName(event) {
-    event.preventDefault();
-    let cityInput = document.querySelector("#city-input");
-
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  let updatedCity = document.querySelector("#updated-city");
+  updatedCity.innerHTML = cityInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit" changeCityName);
+searchFormElement.addEventListener("submit", changeCityName);
